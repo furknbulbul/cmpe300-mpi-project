17fafc031c840e0d44c09dee0a52768833d0d948
Furkan Bulbul <furkan@Furkans-MacBook-Air.local>
2022-12-12 20:29:21 +0300

    add function

--- main.py
+++ main.py
@@ -8,6 +8,35 @@ rank = comm.Get_rank()
 Nworkers = size-1
 
 
+
+def evaluate_test_data(test_lines, acc_bigram, acc_unigram):
+    for test in test_lines:
+        test = test.strip()
+        test_unigram=test.split()[0]
+        test_bigram=(test.split()[0], test.split()[1])
+        try:
+            probability=acc_bigram[test_bigram]/acc_unigram[test_unigram]
+        except: 
+            probability = 0
+        print("probability of bigram \"{}\" is {}".format(test, probability))
+
+def master_merge(acc_bigram, acc_unigram):
+    
+    for i in range(1, size):
+        received_unigrams=comm.recv(source=i, tag=21)
+        received_bigrams=comm.recv(source=i, tag=22)
+
+        for unigram in received_unigrams.items():
+            word = unigram[0]
+            freq = unigram[1]
+            acc_unigram[word] = acc_unigram.get(word, 0) + freq
+        
+        for bigram in received_bigrams.items():
+            couple_word = bigram[0]
+            freq = bigram[1]
+            acc_bigram[couple_word] = acc_bigram.get(couple_word, 0) + freq
+
+
 #evenly send the numbers of lines to the workers
 def distribute_lines(line_count):
     data_counts = [line_count//Nworkers for i in range(Nworkers)]
@@ -21,7 +50,8 @@ def distribute_lines(line_count):
 #master process
 if rank == 0:
     
-    sample_name = sys.argv[1]
+    #args: sample_name, merge_method, test_name
+    sample_name = sys.argv[1] 
     merge_method = sys.argv[2]
     test_name = sys.argv[3]
 
@@ -31,40 +61,17 @@ if rank == 0:
     test_file = open(test_name, 'r')
     test_lines = test_file.readlines()
     
-    distribute_lines(len(lines))
-    
+    distribute_lines(len(lines))#arrange line counts
     
     for i in range(len(lines)):
         comm.send(lines[i], dest=i%Nworkers+1, tag=i) #send lines one by one to the workers
     
+    acc_bigram ={}
+    acc_unigram ={}
     if (merge_method == "MASTER"):
-        acc_unigram = {}
-        acc_bigram = {}
-        
-        for i in range(1, size):
-            received_unigrams=comm.recv(source=i, tag=21)
-            received_bigrams=comm.recv(source=i, tag=22)
-
-            for unigram in received_unigrams.items():
-                word = unigram[0]
-                freq = unigram[1]
-                acc_unigram[word] = acc_unigram.get(word, 0) + freq
-            
-            for bigram in received_bigrams.items():
-                couple_word = bigram[0]
-                freq = bigram[1]
-                acc_bigram[couple_word] = acc_bigram.get(couple_word, 0) + freq
-
+        master_merge(acc_bigram, acc_unigram)
     
-    for test in test_lines:
-        test = test.strip()
-        test_unigram=test.split()[0]
-        test_bigram=(test.split()[0], test.split()[1])
-        try:
-            probability=acc_bigram[test_bigram]/acc_unigram[test_unigram]
-        except: 
-            probability = 0
-        print("probability of bigram \"{}\" is {}".format(test, probability))
+    evaluate_test_data(test_lines, acc_bigram, acc_unigram) #evaluate probabilities
 
 else:
     print("worker rank:"+ str(rank))
